1551dca59d66ef7836bce922809be0e112dc9da3
Pierrousson <pierre.husson.56@gmail.com>
2026-06-01 01:15:48 +0200

    changements

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ st.set_page_config(page_title="Catalogue Arterm", layout="wide")
 
 SPREADSHEET = "https://docs.google.com/spreadsheets/d/10XWqZyB0ADl5Fxu-3H6BGFd2Bgualee9A_0ZAo2nE5c/edit?gid=0#gid=0"
 REF_SHEETS = {
-    "Rôles": "REF_roles",
     "Types de personnes": "REF_types",
     "PlaceName": "REF_placeName"
 }
--- modules/import_xml.py
+++ modules/import_xml.py
@@ -3,14 +3,7 @@ import xml.etree.ElementTree as ET
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-
-SPREADSHEET = "https://docs.google.com/spreadsheets/d/10XWqZyB0ADl5Fxu-3H6BGFd2Bgualee9A_0ZAo2nE5c/edit?gid=0#gid=0"
-
-REF_SHEETS = {
-    "Rôles": "REF_roles",
-    "Types de personnes": "REF_types",
-    "PlaceName": "REF_placeName"
-}
+from app import SPREADSHEET, REF_SHEETS
 
 ns = {"tei": "http://www.tei-c.org/ns/1.0"}
 
