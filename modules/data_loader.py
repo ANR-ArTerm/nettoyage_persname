502669aaa386a79ff615dd2d12e9895bbed4622d
Pierrousson <pierre.husson.56@gmail.com>
2026-06-01 01:20:35 +0200

    data loader

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from config import REF_SHEETS
+
 @st.cache_data(ttl=60)
 def load_data(_conn, spreadsheet):
     df = _conn.read(spreadsheet=spreadsheet)
@@ -8,9 +10,12 @@ def load_data(_conn, spreadsheet):
 # Chargement des listes de référence (une seule fois au démarrage)
 @st.cache_data
 def load_ref_lists(_conn, spreadsheet):
-    roles = _conn.read(spreadsheet=spreadsheet, worksheet="REF_roles", usecols=[0], ttl=60)
-    types = _conn.read(spreadsheet=spreadsheet, worksheet="REF_types", usecols=[0], ttl=60)
+    types = _conn.read(
+        spreadsheet=spreadsheet,
+        worksheet=REF_SHEETS["Types de personnes"],
+        usecols=[0],
+        ttl=60,
+    )
     return {
-        "roles": roles.iloc[:, 0].dropna().tolist(),
         "types": types.iloc[:, 0].dropna().tolist(),
     }
